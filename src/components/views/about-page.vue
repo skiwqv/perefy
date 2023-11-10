aabc72bfef32cf361a9862ad64473f6ac035d96f
skiwqv <andreus0212@gmail.com>
2023-11-10 14:46:24 +0200

    add music lite

--- src/components/views/about-page.vue
+++ src/components/views/about-page.vue
@@ -1,8 +1,10 @@
 
 <template>
   <div>
+    <audio controls :src="playSong"></audio>
     <div v-for="song in musicName" :key="song.id">
-      <div>{{song.name}}</div>
+      <div @click="selectSong(song)">{{song.name}}</div>
+      <button>Play</button>
     </div>
   </div>
 </template>
@@ -14,15 +16,35 @@ const axios = require('axios');
 
 const options = {
   method: 'GET',
-  url: ' https://thibaultjanbeyer.github.io/YouTube-Free-Audio-Library-API/api.json',
+  url: 'https://thibaultjanbeyer.github.io/YouTube-Free-Audio-Library-API/api.json',
 };
 
 
 const musicName = ref({})
+const songMap = ref({})
+const songId = ref()
+const songMapKeys = ref({})
+
 async function fetchMusic(){
   const response = await axios.request(options);
   musicName.value = response.data.all
+  songMap.value = response.data.map
+  songMapKeys.value = Object.entries(songMap.value)
+  console.log('songMapKeys.value',songMapKeys.value);
 }
+
+
+const songData = ref({})
+let playSong = ref()
+
+function selectSong(song) {
+  songData.value = song;
+  songId.value = songData.value.id
+  const alligator = songMap.value
+  playSong.value = alligator[songId.value]
+  console.log(playSong.value );
+}
+
 onMounted(()=>{
   fetchMusic()
 })
