1b2f9f9521a097449e7cfb73c3d66a1b3e907b52
skiwqv <andreus0212@gmail.com>
2023-11-10 09:30:51 +0200

    find music api

--- src/components/views/about-page.vue
+++ src/components/views/about-page.vue
@@ -1,11 +1,32 @@
+
 <template>
-  <div>about</div>
+  <div>
+    <div v-for="song in musicName" :key="song.id">
+      <div>{{song.name}}</div>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import {ref, onMounted} from 'vue'
+const axios = require('axios');
 
+
+const options = {
+  method: 'GET',
+  url: ' https://thibaultjanbeyer.github.io/YouTube-Free-Audio-Library-API/api.json',
+};
+
+
+const musicName = ref({})
+async function fetchMusic(){
+  const response = await axios.request(options);
+  musicName.value = response.data.all
 }
+onMounted(()=>{
+  fetchMusic()
+})
+
 </script>
 
 <style>
